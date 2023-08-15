d8d511aa52d1cc8ddf5f01e58455b840512cec9d
exile213 <exilegamer3@gmail.com>
2023-08-15 23:23:00 +0800

    Pass 02_repeatString(Test1)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let words ="";
+    for(let i=1;i<=times;i++){
+        words += word;
+    }
+    return words;
 };
 
 // Do not edit below this line
