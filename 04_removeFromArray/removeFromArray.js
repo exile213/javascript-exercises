2d7f692059757932e8ce9a73c2f9d153ff0ab611
exile213 <exilegamer3@gmail.com>
2023-08-16 22:18:01 +0800

    Pass 04_removeFromArray.js(Test1)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(Arrays, ...Arguements) {
 
+    for(const argue of Arguements) {
+       Arrays = Arrays.filter(function(Array){
+            return Array !== argue;
+            }
+        );
+    };
+    return Arrays
 };
 
 // Do not edit below this line
