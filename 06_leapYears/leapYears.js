ca48c9d7b8379f7692cc99cda1bb452169c1cd18
exile213 <exilegamer3@gmail.com>
2023-08-17 21:11:04 +0800

    Pass 06_leapYears(Test1)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    let leapyear=true;
 
+        if(year%4 ==0){
+            return leapyear = true;
+        }
+        else if(year%100 ==0 && year%400 ==0){
+            return leapyear = true;
+        }
+        else{
+            return leapyear = false;
+        }
 };
 
 // Do not edit below this line
