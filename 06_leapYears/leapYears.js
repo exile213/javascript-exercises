8450c871c40b1b6e3faebbad7976d63e4585ac67
exile213 <exilegamer3@gmail.com>
2023-08-17 21:26:36 +0800

    Update Function Conditionals, Pass 06_leapYears(Test4)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,10 +3,15 @@ const leapYears = function(year) {
     let leapyear=true;
 
         if(year%4 ==0){
-            return leapyear = true;
-        }
-        else if(year%100 ==0 && year%400 ==0){
-            return leapyear = true;
+            if(year%100 !=0 && year%400 !=0){
+                return leapyear = true;
+            }
+            else if(year%100 ==0 && year%400 ==0){
+               return leapyear = true;
+            }
+            else{
+                return leapyear = false;
+            }
         }
         else{
             return leapyear = false;
