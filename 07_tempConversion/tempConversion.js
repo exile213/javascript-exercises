4fb4848cc124b9e22110b6e0664dcc84cd3425b3
exile213 <exilegamer3@gmail.com>
2023-08-17 21:52:29 +0800

    Pass 07_tempConversion(Test1)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+
+  let ToCelsius= ((Fahrenheit- 32) * 5/9);
+
+  return Math.round(ToCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+let ToFahrenheit= ((Celsius* 9 / 5) + 32);
+
+return Math.round(ToFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
